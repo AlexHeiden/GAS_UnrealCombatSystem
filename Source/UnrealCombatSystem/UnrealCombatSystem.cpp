2c5555caf2b1d1dce371427e40299dbf8dd0cf33
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-15 11:19:06 +0200

    Create Unreal project with required plugins

---
+++ Source/UnrealCombatSystem/UnrealCombatSystem.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealCombatSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCombatSystem, "UnrealCombatSystem" );
+
+DEFINE_LOG_CATEGORY(LogUnrealCombatSystem)
