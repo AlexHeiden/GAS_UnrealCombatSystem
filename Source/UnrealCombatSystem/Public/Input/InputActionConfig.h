d89eb31f4ba326de902512f88521bc0684022eca
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-18 13:01:18 +0200

    Create Input Action Config data asset for tag-based ability activation

---
+++ Source/UnrealCombatSystem/Public/Input/InputActionConfig.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "GameplayTagContainer.h"
+#include "InputAction.h"
+#include "InputActionConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FInputActionMapping
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	const UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag;
+};
+
+UCLASS()
+class UInputActionConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FInputActionMapping> InputActions;
+};
