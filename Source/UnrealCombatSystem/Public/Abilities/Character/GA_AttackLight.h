f79baf1a032de70a0d3bcb0668caa2b19f977122
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-19 12:45:18 +0200

    Create GA_AttackLight

---
+++ Source/UnrealCombatSystem/Public/Abilities/Character/GA_AttackLight.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Abilities/BaseGameplayAbility.h"
+#include "GA_AttackLight.generated.h"
+
+UCLASS()
+class UGA_AttackLight: public UBaseGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGA_AttackLight();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags = nullptr,
+		const FGameplayTagContainer* TargetTags = nullptr,
+		FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+	UFUNCTION(BlueprintCallable, Category = "Ability")
+	void EndAttackLight();
+};
