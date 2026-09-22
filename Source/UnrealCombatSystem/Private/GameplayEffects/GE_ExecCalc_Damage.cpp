070b905dc2b8840f469b1c054d2c41221775a342
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-22 12:27:50 +0200

    Create UGE_ExecCalc_Damage class for safe Damage applying

---
+++ Source/UnrealCombatSystem/Private/GameplayEffects/GE_ExecCalc_Damage.cpp
@@ -0,0 +1,11 @@
+﻿#include "GameplayEffects/GE_ExecCalc_Damage.h"
+
+UGE_ExecCalc_Damage::UGE_ExecCalc_Damage()
+{
+}
+
+void UGE_ExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+}
---
+++ Source/UnrealCombatSystem/Public/GameplayEffects/GE_ExecCalc_Damage.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "GameplayEffectExecutionCalculation.h"
+#include "GE_ExecCalc_Damage.generated.h"
+
+
+UCLASS()
+class UGE_ExecCalc_Damage: public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	UGE_ExecCalc_Damage();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+		FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
