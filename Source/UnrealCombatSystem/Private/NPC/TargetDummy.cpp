ae9b8e060bf29d51f7c9cf027f938372bc14ecca
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-24 15:53:19 +0200

    Add TargetDummy class and BP to receive damage

---
+++ Source/UnrealCombatSystem/Private/NPC/TargetDummy.cpp
@@ -0,0 +1,21 @@
+﻿#include "NPC/TargetDummy.h"
+
+
+ATargetDummy::ATargetDummy()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	HealthAttributeSet = CreateDefaultSubobject<UHealthAttributeSet>(TEXT("HealthAttributeSet"));
+
+	if (HealthAttributeSet)
+	{
+		UE_LOG(LogTemp, Display, TEXT("%s Health: %f / MaxHealth: %f"),
+		*GetName(), HealthAttributeSet->GetHealth(), HealthAttributeSet->GetMaxHealth());
+	}
+}
+
+UAbilitySystemComponent* ATargetDummy::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/UnrealCombatSystem/Public/NPC/TargetDummy.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSets/HealthAttributeSet.h"
+#include "TargetDummy.generated.h"
+
+UCLASS()
+class ATargetDummy: public AActor, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+public:
+	ATargetDummy();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities")
+	TObjectPtr<UHealthAttributeSet> HealthAttributeSet;
+};
