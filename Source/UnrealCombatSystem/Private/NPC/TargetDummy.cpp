30bffca1b593fb14a548f269c5bc363faf8dd12c
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-25 15:35:43 +0200

    Delete redundant TargetDummy logs

--- Source/UnrealCombatSystem/Private/NPC/TargetDummy.cpp
+++ Source/UnrealCombatSystem/Private/NPC/TargetDummy.cpp
@@ -7,12 +7,6 @@ ATargetDummy::ATargetDummy()
 	AbilitySystemComponent->SetIsReplicated(true);
 
 	HealthAttributeSet = CreateDefaultSubobject<UHealthAttributeSet>(TEXT("HealthAttributeSet"));
-
-	if (HealthAttributeSet)
-	{
-		UE_LOG(LogTemp, Display, TEXT("%s Health: %f / MaxHealth: %f"),
-		*GetName(), HealthAttributeSet->GetHealth(), HealthAttributeSet->GetMaxHealth());
-	}
 }
 
 UAbilitySystemComponent* ATargetDummy::GetAbilitySystemComponent() const
