bd267818c63973efe5894def6d401c88477e85c0
Aleksei Stepanov <73959167+AlexHeiden@users.noreply.github.com>
2026-09-25 17:03:43 +0200

    Create UAnimNotifyState_HitWindow

---
+++ Source/UnrealCombatSystem/Private/Animation/UAnimNotifyState_HitWindow.cpp
@@ -0,0 +1,38 @@
+﻿#include "Animation/UAnimNotifyState_HitWindow.h"
+
+#include "GameplayTagContainer.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
+void UAnimNotifyState_HitWindow::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                             float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;	
+	}
+
+	FGameplayTag OpenTag = FGameplayTag::RequestGameplayTag(FName("Event.HitWindow.Open"));
+	FGameplayEventData EventData;
+	EventData.EventTag = OpenTag;
+	EventData.Instigator = Owner;
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, OpenTag, EventData);
+}
+
+void UAnimNotifyState_HitWindow::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;	
+	}
+
+	FGameplayTag CloseTag = FGameplayTag::RequestGameplayTag(FName("Event.HitWindow.Close"));
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, CloseTag, FGameplayEventData());
+}
---
+++ Source/UnrealCombatSystem/Public/Animation/UAnimNotifyState_HitWindow.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "UAnimNotifyState_HitWindow.generated.h"
+
+
+UCLASS()
+class UAnimNotifyState_HitWindow: public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+};
